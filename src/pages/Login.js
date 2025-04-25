4e227e60cb18835f0ad45ad60c15b0b9a0234a32
Jenny <vannej321@gmail.com>
2025-04-24 20:40:41 -0500

    Se arreglaron los link

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
     
-        const response = await fetch("http://parcialback-five.vercel.app/api/users/login", {
+        const response = await fetch("https://parcialback-five.vercel.app/api/users/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
